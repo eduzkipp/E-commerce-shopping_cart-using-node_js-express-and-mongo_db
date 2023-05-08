6b67e7ed2263e5b6c26a5f4818866110730365f3
Edwin <eduzkipp@gmail.com>
2023-05-08 10:21:03 +0300

    8/5/2023

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var express=require('express');
 var path=require('path');
+var mongoose=require('mongoose');
+var config=require('./config/database');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(config.database);
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+console.log("Database connected successfully");
+}
+
 //init app
 var app=express();
 //view engine
